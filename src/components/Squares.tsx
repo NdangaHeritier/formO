e7175a34a292f193041eae6fdbf58c50f8d5da5d
@Ndanga <ndangahertier@gmail.com>
2025-05-14 16:00:26 +0200

    adding squares

--- src/components/Squares.tsx
+++ src/components/Squares.tsx
@@ -1,13 +1,12 @@
 type Props = {
     parLength: number;
-    cols: number;
 };
 export const Squares = ({parLength}: Props) => {
     
     return (
-        <div className={`bg-yellow-600/10 w-full h-full absolute right-0 top-0 gap-0.5 z-0 grid grid-cols-12 max-sm:grid-cols-4 p-0 overflow-hidden`}>
+        <div className={`bg-transparent w-full h-full absolute right-0 top-0 z-0 grid grid-cols-12 max-sm:grid-cols-4 p-0 overflow-hidden`}>
             {Array.from({length: parLength}, (_, i) => (
-                <span className="bg-zinc-600/10" key={i}></span>
+                <span className="bg-transparent border border-white/5" key={i}></span>
             ))}
         </div>    
 )
