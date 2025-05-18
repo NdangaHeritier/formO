73e1bedf9501a84cafb7e635958f8fed5d21d917
alituanfamous <famousalituan@gmail.com>
2025-05-18 15:06:32 +0200

    Update YellowBorderedSpan.tsx

--- src/components/Global/YellowBorderedSpan.tsx
+++ src/components/Global/YellowBorderedSpan.tsx
@@ -7,7 +7,7 @@ type Props = {
 const YellowBorderedSpan = ({showIcon, title}: Props) => {
     return (
         <>
-            <div className="bg-gradient-to-l from-yellow-700 from-20% to-yrllow-700/5 rounded-full p-[0.5px]">
+            <div className="bg-gradient-to-l from-yellow-700 from-20% to-yellow-700/5 rounded-full p-[0.5px]">
                 <div className="bg-black rounded-full">
                     <div className="bg-yellow-700/10 rounded-full font-semibold p-2 flex items-center justify-center gap-1 text-sm">
                         <span className="text-zinc-500 px-2">{title}</span>
