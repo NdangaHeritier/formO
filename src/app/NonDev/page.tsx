6f339dc959cc70beb251026f9375c59d2dfdf325
@Ndanga <ndangahertier@gmail.com>
2025-06-07 01:32:10 +0200

    Update page.tsx

--- src/app/NonDev/page.tsx
+++ src/app/NonDev/page.tsx
@@ -2,12 +2,14 @@ import { HeroSection } from "@/components/Global/HeroSection";
 import Solutions from "@/components/Global/Solutions";
 import Features from "@/components/Global/Features";
 import WhyChooseus from "@/components/Global/WhyChooseus";
+import Header from "@/components/Header";
 
 
 
 export default function NonDev() {
   return (
     <div className="w-full flex flex-col justify-start gap-5">
+      <Header />
 
         <div className="px-5">
           <HeroSection  />
